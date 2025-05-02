0fafb9ea1c5b8e562bae47b50ea04d7a13fc0ce9
Dan <danangelodemontano13@gmail.com>
2025-05-02 16:43:21 +0800

    Contents added on Dashboard, added new CSS for index table and updated layouts.

--- INFOP2/obj/Debug/net9.0/INFOP2.AssemblyInfo.cs
+++ INFOP2/obj/Debug/net9.0/INFOP2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFOP2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0766ae8cda447a4c77a48005708ba4c46d95af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+570192ef631a7b2c298fe53c628547b52610fa2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFOP2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFOP2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
