1add5e1c90a9e5f14bdbba11f12761c09d68095d
Dan <danangelodemontano13@gmail.com>
2025-05-07 20:08:44 +0800

    refactor(js): transfer scripts intro dedicated file for cleaner structure

---
+++ INFOP2/wwwroot/js/prayer-ministry.js
@@ -0,0 +1,7 @@
+﻿// Load event data into edit modal
+function loadEventForEdit(id, title, notes, remarks) {
+    document.getElementById("editEventId").value = id;
+    document.getElementById("editEventTitle").value = title;
+    document.getElementById("editEventNotes").value = notes;
+    document.getElementById("editEventRemarks").value = remarks;
+}
