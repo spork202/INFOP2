9624fbb7fa17822394be604f8f97ee58a9614b44
Dan <danangelodemontano13@gmail.com>
2025-05-09 20:46:04 +0800

    feat(ui): implmenet User Settings section for account management

---
+++ INFOP2/Pages/UserSettings.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace INFOP2.Pages {
+    public class UserSettings : PageModel {
+        public void OnGet() {
+            
+        }
+    }
+}
---
+++ INFOP2/wwwroot/js/user-settings.js
@@ -0,0 +1,10 @@
+﻿
+    function togglePasswordVisibility(inputId, iconId) {
+    const input = document.getElementById(inputId);
+    const icon = document.getElementById(iconId);
+    const isPassword = input.type === "password";
+    input.type = isPassword ? "text" : "password";
+    icon.classList.toggle("fa-eye");
+    icon.classList.toggle("fa-eye-slash");
+}
+
