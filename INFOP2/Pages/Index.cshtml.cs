7f90e531107258829a1472c600c1ec89ad8deab7
Dan <danangelodemontano13@gmail.com>
2025-05-09 16:19:51 +0800

    wip(js): ongoing updates to script functionality

--- INFOP2/Pages/Index.cshtml.cs
+++ INFOP2/Pages/Index.cshtml.cs
@@ -1,23 +1,34 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace INFOP2.Pages
 {
+    using System.Collections.Generic;
+
     [Authorize]
+    
+    
     public class IndexModel : PageModel
     {
+        public string FirebaseConfigJson { get; set; }
+        private readonly IConfiguration _configuration;
+        
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+    
+        public IndexModel(ILogger<IndexModel> logger , IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-            _logger.LogInformation("Index page accessed by {User}", User.Identity.Name);
-            ViewData["Title"] = "Home page";
+            _logger.LogInformation("Masterlist page accessed by {User}", User.Identity.Name);
+            FirebaseConfigJson = System.Text.Json.JsonSerializer.Serialize(_configuration.GetSection("Firebase").Get<Dictionary<string, string>>());
         }
     }
 }
