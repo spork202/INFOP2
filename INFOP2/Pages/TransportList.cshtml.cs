06f3fc892f758d5a6c2c2714088890f8f0a0368f
Spork202 <gilbolingamarlo@gmail.com>
2025-05-11 17:30:09 +0300

    Transport List connected to firebase

--- INFOP2/Pages/TransportList.cshtml.cs
+++ INFOP2/Pages/TransportList.cshtml.cs
@@ -1,24 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace INFOP2.Pages
 {
     public class TransportList : PageModel
     {
+      
+        private readonly IConfiguration _configuration;
         private readonly ILogger<TransportList> _logger;
 
-        public TransportList(ILogger<TransportList> logger)
+        public string FirebaseConfigJson { get; set; }
+
+        public TransportList(IConfiguration configuration, ILogger<TransportList> logger)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
+            _logger.LogInformation("Masterlist page accessed by {User}", User.Identity.Name);
+            FirebaseConfigJson = System.Text.Json.JsonSerializer.Serialize(_configuration.GetSection("Firebase").Get<Dictionary<string, string>>());
         }
+    }
+
+    public class TransportEvent
+    {
+        public string EventId { get; set; }
+        public string EventName { get; set; }
+        public string DriverName { get; set; }
+        public string Note { get; set; }
+        public string Passengers {get; set;}
+        public string Remarks {get; set;}
+        public string ClosingNote {get; set;}
+        public string BibleVerse {get; set;}
+
     }
 }
