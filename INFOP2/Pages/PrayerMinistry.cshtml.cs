3d9d95473ca216d2036d4d5f992b05efe780fcf2
Spork202 <gilbolingamarlo@gmail.com>
2025-05-11 17:30:37 +0300

    Prayer Ministry connected to firebase

--- INFOP2/Pages/PrayerMinistry.cshtml.cs
+++ INFOP2/Pages/PrayerMinistry.cshtml.cs
@@ -1,24 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace INFOP2.Pages
 {
     public class PrayerMinistry : PageModel
     {
         private readonly ILogger<PrayerMinistry> _logger;
+        private readonly IConfiguration _configuration;
 
-        public PrayerMinistry(ILogger<PrayerMinistry> logger)
+        public PrayerMinistry(ILogger<PrayerMinistry> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
+    public string FirebaseConfigJson{get; set;}
         public void OnGet()
         {
+             _logger.LogInformation("Masterlist page accessed by {User}", User.Identity.Name);
+            FirebaseConfigJson = System.Text.Json.JsonSerializer.Serialize(_configuration.GetSection("Firebase").Get<Dictionary<string, string>>());
         }
+        
+    }
+
+    public class Prayers{
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Notes {get; set; }
+        public string Remarks {get; set; }
     }
 }
