b0a7c66780152ebd4920caf0655affcb7e905f03
Dan <danangelodemontano13@gmail.com>
2025-05-13 11:21:58 +0800

    feat(ui): complete export button additions, integrate new JS files, refine styles

--- INFOP2/wwwroot/js/transport-list.js
+++
@@ -1,6 +0,0 @@
-﻿<!-- JS: Load Driver Info into Edit Modal -->
-function loadDriverForEdit(id, name, contact) {
-    document.getElementById("editDriverId").value = id;
-    document.getElementById("editDriverName").value = name;
-    document.getElementById("editDriverContact").value = contact;
-}
